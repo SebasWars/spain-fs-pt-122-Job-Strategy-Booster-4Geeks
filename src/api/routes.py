8933eaa57484d0254f8da790e8bc776368de3ec0
Sebas <sebastiantorres.develop@gmail.com>
2026-01-20 13:05:06 +0100

    Método post postulaciones

--- src/api/routes.py
+++ src/api/routes.py
@@ -524,3 +524,234 @@ def count_post():
     user = User.query.get(current_user)
     count = Postulaciones.query.filter_by(user_id=user.id).count()
     return jsonify({"count": count})
+
+@api.route("/WorkType/<int:id>", methods=["GET"])
+def work_type_id(id):
+    work_type_id = WorkType.query.filter_by(id=id).first()
+    if not work_type_id:
+        return jsonify({"msg": "new_work_type not found"}), 404
+    return jsonify(work_type_id.serialize())
+
+
+@api.route("/WorkType", methods=["POST"])
+def work_type_post():
+    data = request.get_json()
+    name = data.get("name")
+    if not name:
+        return jsonify({"msg": "This field is required"})
+    new_work_type = WorkType(name=name)
+    db.session.add(new_work_type)
+    db.session.commit()
+    return jsonify(new_work_type.serialize())
+
+
+@api.route("/WorkType/<int:id>", methods=["PUT"])
+def new_work_type_put(id):
+    new_work_type = WorkType.query.filter_by(id=id).first()
+    if not new_work_type:
+        return jsonify({"msg": "new_work_type not founded"})
+    data = request.get_json()
+    name = data.get("name")
+    if not name:
+        return jsonify({"msg": "This field is required"})
+    if name:
+        new_work_type.name = name
+    db.session.commit()
+    return jsonify(new_work_type.serialize())
+
+
+@api.route("/WorkType/<int:id>", methods=["DELETE"])
+def new_work_type_delete(id):
+    new_work_type = WorkType.query.filter_by(id=id).first()
+    if not new_work_type:
+        return jsonify({"msg": "new_work_type not founded"})
+    db.session.remove(new_work_type)
+    db.session.commit()
+    return jsonify({"msg": "This new_work_type was deleted"})
+
+
+# -----------------------------EmploymentType-----------------------------#
+@api.route("/EmploymentType", methods=["GET"])
+def employment_type_get():
+    employment_types = EmploymentType.query.order_by(
+        EmploymentType.id.asc()).all()
+    list_employment_type = [employment_type.serialize()
+                            for employment_type in employment_types]
+    return jsonify(list_employment_type)
+
+
+@api.route("/EmploymentType/<int:id>", methods=["GET"])
+def employment_type_id(id):
+    employment_type = EmploymentType.query.filter_by(id=id).first()
+    if not employment_type:
+        return jsonify({"msg": "employment_type not found"}), 404
+    return jsonify(employment_type.serialize())
+
+
+@api.route("/EmploymentType", methods=["POST"])
+def employment_type_post():
+    data = request.get_json()
+    name = data.get("name")
+    if not name:
+        return jsonify({"msg": "This field is required"})
+    new_employment_type = EmploymentType(name=name)
+    db.session.add(new_employment_type)
+    db.session.commit()
+    return jsonify(new_employment_type.serialize())
+
+
+@api.route("/EmploymentType/<int:id>", methods=["PUT"])
+def employment_type_put(id):
+    employment_type = EmploymentType.query.filter_by(id=id).first()
+    if not employment_type:
+        return jsonify({"msg": "employment_type not founded"})
+    data = request.get_json()
+    name = data.get("name")
+    if not name:
+        return jsonify({"msg": "This field is required"})
+    if name:
+        employment_type.name = name
+    db.session.commit()
+    return jsonify(employment_type.serializer())
+
+
+@api.route("/EmploymentType/<int:id>", methods=["DELETE"])
+def employment_type_delete(id):
+    employment_type = EmploymentType.query.filter_by(id=id).first()
+    if not employment_type:
+        return jsonify({"msg": "employment_type not founded"})
+    db.session.remove(employment_type)
+    db.session.commit()
+    return jsonify({"msg": "This employment_type was deleted"})
+
+
+# -----------------------------Postulaciones-----------------------------#
+@api.route("/posts/my-post-count", methods=["GET"])
+@jwt_required()
+def count_post():
+    current_user = get_jwt_identity()
+    user = User.query.get(current_user)
+    count = Postulaciones.query.filter_by(user_id=user.id).count()
+    return jsonify({"count": count})
+
+@api.route("/posts", methods=["POST"])
+@jwt_required()
+def postulaciones_post():
+
+    data = request.get_json()
+    current_user_id = get_jwt_identity()
+
+    def safe_int(val, default=None):
+        try:
+            return int(val)
+        except (ValueError, TypeError):
+            return default
+
+    company_name = data.get("company_name")
+    expireiance = data.get("expireiance")
+    city_id = safe_int(data.get("city_id"))
+    salary = data.get("salary")
+    platform = data.get("plataforma")
+    postulation_url = data.get("url")
+    category_id = safe_int(data.get("category_id"))
+    work_type_id = safe_int(data.get("work_type_id"))
+    employment_type_id = safe_int(data.get("employment_type_id"))
+    requirements = data.get("requirements")
+    candidates_applied = data.get("candidates_applied")
+    positions = data.get("positions")
+    job_description = data.get("job_description")
+
+
+    if None in [city_id, category_id, work_type_id, employment_type_id]:
+        return {"error": "Invalid or missing foreign key IDs"}, 400
+
+    if not City.query.get(city_id):
+        return {"error": "City not found"}, 404
+    if not Category.query.get(category_id):
+        return {"error": "Category not found"}, 404
+    if not WorkType.query.get(work_type_id):
+        return {"error": "Work type not found"}, 404
+    if not EmploymentType.query.get(employment_type_id):
+        return {"error": "Employment type not found"}, 404
+
+    social_media_list = data.get("social_media", [])
+    social_media_status_entries = []
+
+    for sm in social_media_list:
+        sm_platform = sm.get("platform")
+        sm_status_ids = sm.get("status")
+
+        if sm_platform is None:
+            return {"error": "Missing social media platform id"}, 400
+
+        if not isinstance(sm_status_ids, list):
+            sm_status_ids = [sm_status_ids]
+
+        social_media = SocialMedia.query.get(sm_platform)
+        if not social_media:
+            return {"error": f"Social media platform with id {sm_platform} not found"}, 404
+
+        for status_id in sm_status_ids:
+            sm_status_id = safe_int(status_id)
+            if sm_status_id is None:
+                return {"error": "Invalid social media status id"}, 400
+
+            sm_status = SocialMediaStatus.query.get(sm_status_id)
+            if not sm_status:
+                return {"error": f"Social media status id {sm_status_id} not found"}, 404
+
+            social_media_status_entries.append((social_media.id, sm_status.id))
+
+    skills_ids = [safe_int(i) for i in data.get(
+        "skills", []) if safe_int(i) is not None]
+    skills_objects = Skill.query.filter(Skill.id.in_(skills_ids)).all()
+
+    pending_status = Status.query.filter_by(name="pending").first()
+    if not pending_status:
+        return {"error": "Missing 'pending' status in DB"}, 500
+
+    new_postulacion = Postulaciones(
+        company_name=company_name,
+        expireiance=expireiance,
+        city_id=city_id,
+        salary=salary,
+        platform = platform,
+        postulation_url = postulation_url,
+        status_id=pending_status.id,
+        category_id=category_id,
+        work_type_id=work_type_id,
+        employment_type_id=employment_type_id,
+        requirements=requirements,
+        candidates_applied=candidates_applied,
+        job_description=job_description,
+        positions=positions,
+        user_id=safe_int(current_user_id),
+        skills=skills_objects,
+    )
+
+    db.session.add(new_postulacion)
+    db.session.flush()
+    for social_media_id, social_media_status_id in social_media_status_entries:
+        stmt = postulacion_social_media.insert().values(
+            postulacion_id=new_postulacion.id,
+            social_media_id=social_media_id,
+            social_media_status_id=social_media_status_id,
+        )
+        db.session.execute(stmt)
+
+    user = User.query.get(new_postulacion.user_id)
+    profile = Profile.query.filter_by(
+        user_id=user.id).first() if user else None
+
+    post_skill_ids = {skill.id for skill in skills_objects}
+    profile_skill_ids = {
+        skill.id for skill in profile.skills} if profile else set()
+
+    matched_count = len(post_skill_ids & profile_skill_ids)
+    matched_percentage = (matched_count / len(post_skill_ids)
+                          * 100) if post_skill_ids else 0
+    new_postulacion.matched_percentage = round(matched_percentage, 2)
+
+    db.session.commit()
+
+    return jsonify(new_postulacion.serialize()), 201
