e4a3fdbd134a8f1c14bb49288e9d85740f2b798f
Tsvet <youremail@example.com>
2026-01-21 16:57:01 +0100

    Cambios

--- src/front/components/JobCard2.jsx
+++ src/front/components/JobCard2.jsx
@@ -1,25 +1,27 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLocationDot, faCalendarDays, faEuroSign, faPeopleGroup, faClipboardList } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 
 
-function JobCard2() {
+function JobCard2({post}) {
+    const navigate=useNavigate()
     return (
-        <div className="card_container">
+        <div onClick={()=>navigate(`/postulacion/${post.id}`)} className="card_container">
 
             <div className="card_header">
                 <div className="header_title">
-                    <span className="status">Open</span>
-                    <span className="category">Developer</span>
+                    <span className="status">{post.status}</span>
+                    <span className="category">{post.company}</span>
                 </div>
                 <button className="view_details">Detalles</button>
             </div>
 
             <div className="application_card_title">
                 <div className="title">
-                    <h2>React developer</h2>
+                    <h2>{post.title}</h2>
                     <div className="created_at">
-                        <h4><FontAwesomeIcon icon={faLocationDot} />Location</h4>
-                        <h4><FontAwesomeIcon icon={faCalendarDays} />Ene 20,2026</h4>
+                        <h4><FontAwesomeIcon icon={faLocationDot} />{post.location}</h4>
+                        <h4><FontAwesomeIcon icon={faCalendarDays} />{post.posted_date}</h4>
                     </div>
                     <div className="progres_chart">
 
@@ -32,22 +34,22 @@ function JobCard2() {
                 <div className="application_details">
                     <div className="app_stat salary">
                         <p><FontAwesomeIcon icon={faEuroSign} />Salario</p>
-                        <span className="data">€30k/anual</span>
+                        <span className="data">{post.salary}</span>
                     </div>
                     <div className="app_stat candidates">
                         <p><FontAwesomeIcon icon={faPeopleGroup} />Candidatos</p>
-                        <span className="data">20</span>
+                        <span className="data">{post.candidates_applied}</span>
                     </div>
                     <div className="app_stat application_status">
                         <p><FontAwesomeIcon icon={faClipboardList} />Proceso</p>
-                        <span className="data">Entrevista Tectinca</span>
+                        <span className="data">{post.proceso}</span>
                     </div>
                 </div>
                 <div className="application_tags">
-                    <span style={{ "--tag-color": "var(--job-type)" }}>Remoto</span>
-                    <span style={{ "--tag-color": "var(--contract-type)" }}>M Tiempo</span>
-                    <span style={{ "--tag-color": "var(--experience)" }}>2 Años</span>
-                    <span style={{ "--tag-color": "var(--slots)" }}>2 Plazas</span>
+                    <span style={{ "--tag-color": "var(--job-type)" }}>{post.work_type}</span>
+                    <span style={{ "--tag-color": "var(--contract-type)" }}>{post.tiempo}</span>
+                    <span style={{ "--tag-color": "var(--experience)" }}>{post.experience}</span>
+                    <span style={{ "--tag-color": "var(--slots)" }}>{post.total_positions} Plazas</span>
                 </div>
             </div>
         </div>
